2b42c00751578208d5cee9b01eefd3f16fcca33b
GabrielMiranda05 <qwerlolgabriel@gmail.com>
2023-08-10 19:12:10 -0300

    testes js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//TESTES JS SAIDA
+
+//escreve direto no html
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!!";
+
+
+//mais usado para testes
+document.write('<u> Teste document write <br>')
+
+document.write(5+5)
+
+
+//sai alerta no site
+alert(100+100)
+
+
+//Sai no console do site
+console.log('Oi, isso e um console.log')
+
+
+
+
